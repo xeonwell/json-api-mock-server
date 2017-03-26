770d3fe887a97ec02f708c889c424321e1eee709
xeonwell <admin@xw.hb.cn>
2017-03-26 20:08:05 +0800

    handle proxy error

--- ms/mockServer.js
+++ ms/mockServer.js
@@ -2,15 +2,22 @@
  * Created by xeonwell on 2017-03-11.
  */
 
-let debug = require('debug')('MockServer:server');
-let config = require('./config');
-let httpProxy = require('http-proxy');
-let proxy = httpProxy.createProxyServer({});
+let debug          = require('debug')('MockServer:server');
+let config         = require('./config');
+let httpProxy      = require('http-proxy');
+let proxy          = httpProxy.createProxyServer({});
 let mockUrlHandler = require('./mockUrlHandler');
-let funcs = require('./funcs');
-let parseUrl = require('parseurl');
+let funcs          = require('./funcs');
+let parseUrl       = require('parseurl');
 
 debug('proxy -> ' + config.proxyOptions.target);
+
+// Listen for the `error` event on `proxy`.
+proxy.on('error', function (err, req, res) {
+  res.status(500)
+    .header('Content-Type', 'application/json')
+    .json(funcs.fail('server 500 error'));
+});
 
 module.exports = function (req, res, next) {
 
@@ -31,12 +38,12 @@ module.exports = function (req, res, next) {
   if (objUrl && objUrl.pathname && objUrl.pathname.length > 0) {
     mockUrlHandler(objUrl, req).then(function (json) {
       debug('mock json found, output...');
-      res.json(funcs.success(json))
+      res.json(funcs.success(json));
     }).catch(function (error) {
       // debug('not found, ~> proxy');
-      proxy.web(req, res, config.proxyOptions)
-    })
+      proxy.web(req, res, config.proxyOptions);
+    });
   }
 
   // next();
-}
+};
